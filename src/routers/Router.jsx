d017dc242695db8b44af137bbe3abdc1a519f562
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-10-09 14:27:28 -0500

    Refactor: Al ingresar una ruta no definida se redireccionara automaticamente a la ruta raiz

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Library from "../pages/Library";
 import Archievements from "../pages/Archievements";
 import Stats from "../pages/Stats";
@@ -10,7 +10,7 @@ const Router = () => {
                 <Route path="/" element={<Library />} />
                 <Route path="/stats" element={<Stats />} />
                 <Route path="/archievements" element={<Archievements />} />
-                <Route path="*" element={<Library />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </BrowserRouter>
     );
