8879b7a0d0e6b442d5cc20e926173316f6dea630
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-10-11 12:22:50 -0500

    feat(navigation): Integración de react-router-dom para la navegación.
    - Refactorización del componente RegularBttn para gestionar la navegación dinámica según la ruta actual y el contexto del botón
    - Trasladar BrowserRouter a ViewsIndex y actualizar las rutas de Router para mayor consistencia, cambiando la redirección predeterminada a /library
    - Mejorar los estilos de botones y encabezados para una interfaz de usuario optimizada.
    
    CAMBIO IMPORTANTE: Se actualizaron las rutas (p. ej., / a /library, /stats, /achievements) y la redirección predeterminada, lo que podría afectar a los enlaces de navegación existentes.

--- src/pages/ViewsIndex.jsx
+++ src/pages/ViewsIndex.jsx
@@ -1,13 +1,16 @@
 import Router from "../routers/Router";
 import Header from "../components/header/Header"
 import styles from "../styles/modules/pages/ViewsIndex.module.css";
+import { BrowserRouter } from "react-router-dom";
 
 const ViewsIndex = () => {
     return (
-        <div className={styles.viewsIndex}>
-            <Header />
-            <Router />
-        </div>
+        <BrowserRouter>
+            <div className={styles.viewsIndex}>
+                <Header />
+                <Router />
+            </div>
+        </BrowserRouter>
     );
 };
 
