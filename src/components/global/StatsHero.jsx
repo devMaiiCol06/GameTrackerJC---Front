dc2c2dc15f58171717324088c5b8d0355509102c
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-10-12 20:12:08 -0500

    feat (interfaces): Implementar la integración de datos de logros y la representación de la interfaz de usuario.
    Incorporar llamadas a la API para recuperar datos de logros, gestionar el estado de los componentes para actualizaciones dinámicas y mejorar la interfaz de logros con visualizaciones estadísticas para el seguimiento del progreso. Esta actualización incluye correcciones tipográficas y una estructura de código mejorada para un mejor mantenimiento.

--- src/components/global/StatsHero.jsx
+++ src/components/global/StatsHero.jsx
@@ -7,20 +7,62 @@ const StatsHero = ({ data, context }) => {
             return [
                 {
                     value: data.length,
-                    label: "Total"
+                    label: "Total",
                 },
                 {
-                    value: data.filter((game) => game.gameStatus === "In Progress").length,
-                    label: "In Progress"
+                    value: data.filter(
+                        (game) => game.gameStatus === "In Progress"
+                    ).length,
+                    label: "In Progress",
                 },
                 {
-                    value: data.filter((game) => game.gameStatus === "Completed").length,
-                    label: "Completed"
+                    value: data.filter(
+                        (game) => game.gameStatus === "Completed"
+                    ).length,
+                    label: "Completed",
                 },
                 {
-                    value: data.filter((game) => game.gameStatus === "Wishlist").length,
-                    label: "Wishlist"
-                }
+                    value: data.filter((game) => game.gameStatus === "Wishlist")
+                        .length,
+                    label: "Wishlist",
+                },
+            ];
+        }
+
+        if (context === "archievements") {
+            return [
+                {
+                    value: data.length,
+                    label: "TOTAL",
+                },
+                {
+                    value: data.filter(
+                        (archiev) => archiev.archievStatus === "unlocked"
+                    ).length,
+                    label: "UNLOCKED",
+                },
+                {
+                    value: data.filter(
+                        (archiev) =>
+                            archiev.archievCategory === "legendary" &&
+                            archiev.archievStatus === "unlocked"
+                    ).length,
+                    label: "LEGENDARY",
+                },
+                {
+                    value:
+                        data.length > 0
+                            ? `${Math.round(
+                                  (data.filter(
+                                      (archiev) =>
+                                          archiev.archievStatus === "unlocked"
+                                  ).length /
+                                      data.length) *
+                                      100
+                              )}%`
+                            : "0%",
+                    label: "COMPLETE",
+                },
             ];
         }
 
@@ -34,7 +76,7 @@ const StatsHero = ({ data, context }) => {
     return (
         <div className={styles.StatsHeroContainer}>
             {stats.map((stat, index) => (
-                <div key={index} className={styles.statCounter}>
+                <div key={index} className={`${styles.statCounter} ${styles.context}`}>
                     <h3>{stat.value}</h3>
                     <p>{stat.label}</p>
                 </div>
--- src/interfaces/Archievements.jsx
+++ src/interfaces/Archievements.jsx
@@ -1,11 +1,30 @@
-import styles from '../styles/modules/interfaces/Archievements.module.css'
+import { useEffect, useState } from "react";
+import StatsHero from "../components/global/StatsHero";
+import styles from "../styles/modules/interfaces/Archievements.module.css";
+import { getArchievs } from "../api/apiArchievs.js";
 
 const Archievements = () => {
+    const [archievsData, setArchievsData] = useState([]);
+
+    useEffect(() => {
+        const fetchArchievs = async () => {
+            try {
+                const response = await getArchievs()
+                setArchievsData(response.archievements)
+            } catch (error) {
+                console.error("Error en fetch:", error);
+            }
+        }
+
+        fetchArchievs();
+    }, []);
+
     return (
-        <div className={styles.ArchievementsContainer} >
-            <span>Archievements</span>
+        <div className={styles.ArchievementsContainer}>
+            <InterfaceHero context="archievements" />
+            <StatsHero context="archievements" data={archievsData} />
         </div>
     );
-}
+};
 
-export default Archievements
+export default Archievements;
