203ee0745626730558295b57895810feed498f79
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-11-21 18:56:27 -0500

    dev2.1 | feat (platforms): Añadir componentes para las plataformas de juego (Nintendo, PC, PlayStation, Smartphone, VR, Xbox) y su integración en GameCard

---
+++ src/components/global/platforms/Nintendo.jsx
@@ -0,0 +1,11 @@
+const Nintendo = ({ width, height }) => {
+    return (
+        <img
+            width={width}
+            height={height}
+            src="https://wp.logos-download.com/wp-content/uploads/2021/02/Nintendo_Switch_Logo.svg?dl"
+        />
+    );
+};
+    
+export default Nintendo;
---
+++ src/components/global/platforms/Playstation.jsx
@@ -0,0 +1,9 @@
+const PlayStation = (props) => (
+    <img
+        {...props}
+        src="https://www.svgrepo.com/show/452087/playstation.svg"
+        alt="PlayStation Logo"
+    />
+);
+
+export default PlayStation;
