2df54fc6f5a673e65e80384908ba0a6b5fff9fa3
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-11-21 18:58:29 -0500

    feat (update, platforms): Update from main.
    - Agregar componentes para las plataformas de juego (Nintendo, PC, PlayStation, Smartphone, VR, Xbox) y su integración en GameCard

---
+++ src/components/global/platforms/PC.jsx
@@ -0,0 +1,11 @@
+const PC = (props) => {
+    return (
+        <img
+            {...props}
+            src="https://uxwing.com/wp-content/themes/uxwing/download/computers-mobile-hardware/monitor-color-icon.png"
+            alt="PC Logo"
+        />
+    );
+};
+
+export default PC;
---
+++ src/components/global/platforms/VR.jsx
@@ -0,0 +1,11 @@
+const VR = (props) => {
+    return (
+        <img
+            {...props}
+            src="https://cdn.freebiesupply.com/logos/large/2x/vr-1-logo-svg-vector.svg"
+            alt="VR Logo"
+        />
+    );
+};
+
+export default VR;
